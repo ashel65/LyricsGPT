2e226e82a93345a54e859dacb5dccdd52f9e6f99
Alexander Shelton <alexshel65@gmail.com>
2023-02-04 16:39:29 -0500

    Separate files and add GPTNeoX boilerplate

--- scrape_lyrics.py
+++ scrape_lyrics.py
@@ -3,7 +3,7 @@ from lyricsgenius import Genius
 
 # parse artist names into array
 with open("rap_artists.txt", "r", encoding="utf-8") as f:
-    artist_names = [i[:-1] for i in f.readlines() if "[" not in i]
+    artist_names = [i[:-1] for i in f.readlines() if "[" not in i][:5]
 
 token = "7Qre4uy98X49_3FsxeXUOtV1ePsxHBZPBIE2xtyHWhHyty1ShrL6nVad7tXh-CFx"
 
@@ -12,5 +12,3 @@ genius = Genius(token)
 for name in artist_names:
     artist = genius.search_artist(name)
     artist.save_lyrics()
-
-generator = pipeline('')
