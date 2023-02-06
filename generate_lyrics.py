e3894e41a0c968dc596f4ff657ddc0bf16826801
Alexander Shelton <alexshel65@gmail.com>
2023-02-06 17:56:30 -0500

    Clean up lyrics generation script

--- generate_lyrics.py
+++ generate_lyrics.py
@@ -1,9 +1,5 @@
-# from transformers import GPTNeoXForCausalLM, GPTNeoXTokenizerFast
-
-# model = GPTNeoXForCausalLM.from_pretrained("EleutherAI/gpt-neox-20b")
-# tokenizer = GPTNeoXTokenizerFast.from_pretrained("EleutherAI/gpt-neox-20b")
-
 from transformers import pipeline, set_seed
+
 generator = pipeline('text-generation', model='/tmp/test-clm')
 set_seed(42)
 print(generator("Hello, I'm a language model,", max_length=30, num_return_sequences=5))
